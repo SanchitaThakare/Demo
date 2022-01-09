da807fcb12603c77fdb78acfdd41c7b6a907ba03
SanchitaThakare <sanchitathakare9@gmail.com>
2022-01-09 20:08:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 export default class componentName extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       email:""
+      email: ""
     }
   }
-  
+
   render() {
+
+
     return (
-      <div> 
-        <input type={'email'} onChange={(event)=>{this.setState({email:event.target.value})}}></input>
-        <button title='Submit' onClick={()=>alert(this.state.email)}></button>
-         </div>
+      <div>
+      <div></div>
+        <input type={'email'} onChange={(event) => { this.setState({ email: event.target.value }) }}></input>
+        <button title='Submit' onClick={() => alert(this.state.email)}></button>
+      </div>
     );
   }
 }
