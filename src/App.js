e99e93d5565d5c4b10cfe4f44a08316e04af78e6
sagarinovantsolution <sagar@inovantsolution.com>
2022-01-09 20:27:10 +0530

    Sagar commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default class componentName extends Component {
 
     return (
       <div>
+        <input type={'email'} onChange={(event) => { this.setState({ email: event.target.value }) }}></input>
         <button title='Submit' onClick={() => alert(this.state.email)}></button>
       </div>
     );
